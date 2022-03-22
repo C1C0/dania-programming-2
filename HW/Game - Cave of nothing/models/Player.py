cd7eee0d8fd7aa6cce1f435a211e5724f23a8ab3
C1C0 <kpskola1002@gmail.com>
2022-03-22 14:25:29 +0100

    Jumping and gravity

--- HW/Game - Cave of nothing/models/Player.py
+++ HW/Game - Cave of nothing/models/Player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from config.settings import PLAYER_SIZES, PLAYER_DEFAULT_SPEED
+from config.settings import PLAYER_SIZES, PLAYER_DEFAULT_SPEED, GRAVITY, PLAYER_JUMP_SPEED
 
 PLAYER_SIGN = 'P'
 
@@ -16,6 +16,9 @@ class Player(pygame.sprite.Sprite):
         
         self.speed = PLAYER_DEFAULT_SPEED
         
+        self.gravity = GRAVITY
+        self.jump_speed = PLAYER_JUMP_SPEED
+        
         
     def getInput(self):
         keys = pygame.key.get_pressed()
@@ -27,6 +30,18 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
             
+        if keys[pygame.K_SPACE]:
+            self.jump()
+            
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+        
+    def jump(self):
+        self.direction.y = self.jump_speed
+            
     def update(self):
         self.getInput()
         self.rect.x += self.direction.x * self.speed
+        
+        self.apply_gravity()
