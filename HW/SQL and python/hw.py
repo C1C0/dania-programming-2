8e56df73b5d5e2fceecf1abebe941345477c1b69
C1C0 <kpskola1002@gmail.com>
2022-03-25 09:55:54 +0100

    Add one table and show them

--- HW/SQL and python/hw.py
+++ HW/SQL and python/hw.py
@@ -13,4 +13,12 @@ cursor: MySQLCursor = con.cursor()
 DB_NAME = "pythondb"
 cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
 
+DB_TABLE_CUSTOMERS = 'customers'
+cursor.execute(f"USE {DB_NAME}")
+cursor.execute(f"CREATE TABLE IF NOT EXISTS {DB_TABLE_CUSTOMERS} (name VARCHAR(255), address VARCHAR(255))")
+cursor.execute("SHOW TABLES")
+
+for x in cursor:
+    print(x)
+
 con.close()
